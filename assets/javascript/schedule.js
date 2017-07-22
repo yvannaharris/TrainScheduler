e1c3bf71b3f9a011167e98e2d225261ff7f95415
Yvanna Harris <yvannaharris@yvannas-air.attlocal.net>
2017-07-21 20:09:37 -0400

    Completed UI

---
+++ assets/javascript/schedule.js
@@ -0,0 +1,12 @@
+var config = {
+    apiKey: "AIzaSyBtaGiFrqV9nBWgDIEclI1HSpphT9iGCOg",
+    authDomain: "trainscheduler-b2a7e.firebaseapp.com",
+    databaseURL: "https://trainscheduler-b2a7e.firebaseio.com",
+    projectId: "trainscheduler-b2a7e",
+    storageBucket: "",
+    messagingSenderId: "968256562058"
+  };
+  
+  firebase.initializeApp(config);
+
+var database.firebase.database();
