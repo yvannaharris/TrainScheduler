bd1734309ba1bbe1ae43b1e5efda1388fa4f90d5
Yvanna Harris <yvannaharris@yvannas-air.attlocal.net>
2017-07-23 00:03:26 -0400

    Added time conversion

--- assets/javascript/schedule.js
+++ assets/javascript/schedule.js
@@ -9,7 +9,7 @@ var config = {
   
   firebase.initializeApp(config);
 
-var database.firebase.database();
+var database = firebase.database();
 
 $("#submit").on("click", function(event) {
   event.preventDefault();
@@ -18,11 +18,50 @@ $("#submit").on("click", function(event) {
   var fristTrainTime = $("#first-train").val().trim();
   var trainFreq = $("#frequency").val().trim();
 
-  database.ref().push({
-    traiName: trainName,
-    trainDestination: trainDest,
-    firstTrainTime: firstTrainTime,
-    trainFrequesncy: trainFreq
-  });
+  var newTrain = {
+    name: trainName,
+    destination: trainDest,
+    time: firstTrainTime,
+    frequesncy: trainFreq
+  };
+
+  database.ref().push(newTrain);
+
+  console.log(newTrain.name);
+  console.log(newTrain.destination);
+  console.log(newTrain.time);
+  console.log(newTrain.frequency);
+
+  $("#train-name").val("");
+  $("#destination").val("");
+  $("#first-train").val("");
+  $("#frequency").val("");
 
 });
+
+database.ref().on("child_added", function(childSnapshot, prevChildKey) {
+  var trainName = childSnapshot.val().newTrian.name;
+  var trainDest = childSnapshot.val().newTrain.destination;
+  var firstTrainTime = childSnapshot.val().newTrain.time;
+  var trainFreq = childSnapshot.val().newTrain.frequency;
+
+  var tFrequency = parseInt("tranFreq");
+
+  var firstTrain = moment(firstTrainTime, "HH:mm").subtract(1, "years");
+
+  var currentTime = moment();
+
+  var diffTime = moment().diff(moment(firstTrain), "minutes");
+
+  var tRemainder = diffTime % tFrequency;
+
+  var tMinutesTillTrain = tFrequency - tRemainder;
+
+  var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+
+  $("#train-table > tbody").append("<tr><td>" + trainName + "</td><td>" + trainDest + "</td><td>" + trainFreq + "</td><td>" + moment(nextTrain).format("HH:mm") + "</td><td>" + tMinutesTillTrain + "</td>");
+
+
+});
+
+
