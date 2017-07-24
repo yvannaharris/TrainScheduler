b14a18549c906af90cff8db4ce4eed21510a8a02
Yvanna Harris <yvannaharris@yvannas-air.attlocal.net>
2017-07-23 20:20:52 -0400

    Added moment.js

--- assets/javascript/schedule.js
+++ assets/javascript/schedule.js
@@ -15,22 +15,18 @@ $("#submit").on("click", function(event) {
   event.preventDefault();
   var trainName = $("#train-name").val().trim();
   var trainDest = $("#destination").val().trim();
-  var fristTrainTime = $("#first-train").val().trim();
+  var firstTrainTime = $("#first-train").val().trim();
   var trainFreq = $("#frequency").val().trim();
 
   var newTrain = {
     name: trainName,
     destination: trainDest,
     time: firstTrainTime,
-    frequesncy: trainFreq
+    frequency: trainFreq
   };
 
   database.ref().push(newTrain);
 
-  console.log(newTrain.name);
-  console.log(newTrain.destination);
-  console.log(newTrain.time);
-  console.log(newTrain.frequency);
 
   $("#train-name").val("");
   $("#destination").val("");
@@ -40,12 +36,13 @@ $("#submit").on("click", function(event) {
 });
 
 database.ref().on("child_added", function(childSnapshot, prevChildKey) {
-  var trainName = childSnapshot.val().newTrian.name;
-  var trainDest = childSnapshot.val().newTrain.destination;
-  var firstTrainTime = childSnapshot.val().newTrain.time;
-  var trainFreq = childSnapshot.val().newTrain.frequency;
+  var trainName = childSnapshot.val().name;
+  var trainDest = childSnapshot.val().destination;
+  var firstTrainTime = childSnapshot.val().time;
+  var trainFreq = childSnapshot.val().frequency;
 
-  var tFrequency = parseInt("tranFreq");
+  var tFrequency = parseInt(trainFreq);
+  
 
   var firstTrain = moment(firstTrainTime, "HH:mm").subtract(1, "years");
 
@@ -59,7 +56,7 @@ database.ref().on("child_added", function(childSnapshot, prevChildKey) {
 
   var nextTrain = moment().add(tMinutesTillTrain, "minutes");
 
-  $("#train-table > tbody").append("<tr><td>" + trainName + "</td><td>" + trainDest + "</td><td>" + trainFreq + "</td><td>" + moment(nextTrain).format("HH:mm") + "</td><td>" + tMinutesTillTrain + "</td>");
+  $("#train-table > tbody").append("<tr><td>" + trainName + "</td><td>" + trainDest + "</td><td>" + trainFreq + "</td><td>" + moment(nextTrain).format("hh:mm a") + "</td><td>" + tMinutesTillTrain + "</td>");
 
 
 });
